5e128a6639d24c91b7d44a63fe11b1f8cfd1583b
TanmoySin <tanmoysin1997@gmail.com>
2026-09-05 23:56:00 +0530

    Build Ready

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import Sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
+  build: {
+    target: 'es2015'
+  },
   plugins: [
     react(),
     Sitemap({
